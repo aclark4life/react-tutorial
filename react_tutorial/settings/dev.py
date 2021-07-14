59cfa87159d5501c638fcb742d7a90cbd8e91fcb
Alex Clark <aclark@aclark.net>
2021-07-14 13:06:41 -0400

    wagtail-init by project-makefile

---
+++ react_tutorial/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-%d!$9&lnlc=%23+#iuwtaoe_cv)%x6ok&qq%8!&b8c!=9xq=7i'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
