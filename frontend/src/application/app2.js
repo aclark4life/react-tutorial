b360b58e7c3987e5ccb6e68fced1d53aa7d9cf42
Alex Clark <aclark@aclark.net>
2021-07-15 09:51:28 -0400

    Update

--- frontend/src/application/app2.js
+++
@@ -1,7 +0,0 @@
-import "bootstrap/dist/js/bootstrap.bundle";
-
-import "../components/sidebar";
-
-window.document.addEventListener("DOMContentLoaded", function () {
-  window.console.log("dom ready 2");
-});
